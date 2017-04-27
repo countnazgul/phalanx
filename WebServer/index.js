74ce7f0ae35cf0258c7063e116eaaec9a1334921
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-04-27 05:29:57 +0100

    v0.0.2
    logic for generating where clause

--- WebServer/index.js
+++ WebServer/index.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var async = require('async')
 var express = require('express')
 var app = express()
 const enigma = require('enigma.js');
@@ -12,11 +13,45 @@ app.get('/', function (req, res) {
 app.get('/phalanx/:area/:file', function (req, res) {
   var area = req.params.area
   var file = req.params.file
+  var params = req.query;
+  var whereClause = '';
 
   var obj = JSON.parse(fs.readFileSync('./config/files.json', 'utf8'))
-  console.log(obj.areas[area][file])
+  //console.log(obj.areas[area][file])
 
-  res.send(area + ' --> ' + JSON.stringify(req.query))
+  async.forEachOf(Object.keys(params), function (key, value, callback) {
+      //console.log(params[key] + ' --> ' + value)
+      var localParams = params[key];
+      var localWhere = '';
+
+      if(value != 0) {
+        localWhere = ' and '
+      } 
+
+      localParams = "'" + localParams + "'";
+      localParams = localParams.split(',').join("','");
+      paramsCount = localParams.split(',').length
+      
+      if(paramsCount > 1) {
+        localWhere += ' ' + key + ' in( ' + localParams + ' ) \r\n';
+      } else {
+        localWhere += ' ' + key + ' = ' + localParams + ' \r\n';
+      }
+      
+      whereClause += localWhere;
+
+      callback();
+  }, function (err) {
+    if (err) {
+      console.log('A file failed to process');
+    } else {
+      whereClause = whereClause + ';'
+      console.log(whereClause)
+      res.send(area + ' --> ' + JSON.stringify(req.query))
+    }
+  });
+
+  
 })
 
 app.get('/create', function (req, res) {
